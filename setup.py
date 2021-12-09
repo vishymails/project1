d5b2bea0cd9498aa7b182a4bb0627c7ed7c964c5
vishymails <vishymails@gmail.com>
2021-12-09 16:59:06 +0530

    test and tox  added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    description ="Project case study for Oracle blr",
+    author = "Dr. Rao",
+    packages = find_packages(),
+    license= "MIT"
+)
---
+++ tests/test_config.py
@@ -0,0 +1,11 @@
+
+
+def test_generic() :
+    a = 2
+    b = 2
+    assert a==b
+
+def test_generic1() :
+    a = 2
+    b = 21
+    assert a!=b
