d915466ee17b8c3c21cf96b375bb82d815dfb586
vishymails <vishymails@gmail.com>
2021-12-10 10:44:46 +0530

    flake added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,20 @@
 
+import pytest
+
+
+class NotInRange(Exception) :
+    def __init__(self, message = "Value not in range ") :
+        self.message = message
+        super().__init__(self.message)
+
+
+# def test_exception() :
+#     a = 1
+#     with pytest.raises(ValueError) :
+#         if a not in range(10, 20) :
+#             raise NotInRange
+
+
 
 def test_generic() :
     a = 2
@@ -9,3 +25,6 @@ def test_generic1() :
     a = 2
     b = 21
     assert a!=b
+
+
+
